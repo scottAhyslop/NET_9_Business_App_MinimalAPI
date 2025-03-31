2e15ed2a0fe9923e8e14a63c54f37e73f59bb957
Scott A. Hyslop <sahyslop@gmail.com>
2025-03-31 18:10:16 -0400

    Added core endpoints
    Four core endpoints of GET, POST, PUT and DELETE have been e implemented in an Endpoint container.
    No testing to confirm as routing has not been implemented

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseEndpoints(endpoints =>
+{
+    //GET /employees
+    endpoints.MapGet("/employees", async (HttpContext context) =>
+    { await context.Response.WriteAsync("Get Employees");
+    });//End GET
+
+    //POST /employees
+    endpoints.MapPost("/employees", async (HttpContext context) =>
+     { await context.Response.WriteAsync("Create an Employee");
+     });//End POST
+
+    //PUT /employees
+    endpoints.MapPut("/employees", async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("Update an  Employee");
+    });//End PUT
+
+    //DELETE /employees
+    endpoints.MapDelete("/employees", async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("Delete an Employee");
+    });//End DELETE
+});//End UseEndpoints
 
 app.Run();
